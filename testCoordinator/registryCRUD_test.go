5983b15fd96adab9cd9120fb359d4af726fbd4ef
Guangling.Hastings <hastings_gl@outlook.com>
2017-10-18 00:27:06 -1000

    GoCollaborate ver:0.2.6

---
+++ testCoordinator/registryCRUD_test.go
@@ -0,0 +1,72 @@
+package testCoordinator
+
+import (
+	"github.com/gavv/httpexpect"
+	"net/http"
+	"testing"
+)
+
+func TestRegisterService(t *testing.T) {
+	var (
+		// create httpexpect instance
+		e = httpexpect.New(t, "http://localhost:8080")
+	)
+	// services POST
+	e.POST("/services/{srvid}/registry").
+		Expect().
+		Status(http.StatusOK).JSON()
+}
+
+func TestSubscribeService(t *testing.T) {
+	var (
+		// create httpexpect instance
+		e = httpexpect.New(t, "http://localhost:8080")
+	)
+	// services POST
+	e.POST("/services/{srvid}/subscription").
+		Expect().
+		Status(http.StatusOK).JSON()
+}
+
+func TestDeRegisterService(t *testing.T) {
+	var (
+		// create httpexpect instance
+		e = httpexpect.New(t, "http://localhost:8080")
+	)
+	// DELETE registry
+	e.DELETE("/services/{srvid}/registry/single/{ip}/{port}").
+		Expect().
+		Status(http.StatusOK).JSON()
+}
+
+func TestUnSubscribeService(t *testing.T) {
+	var (
+		// create httpexpect instance
+		e = httpexpect.New(t, "http://localhost:8080")
+	)
+	// DELETE subscription
+	e.DELETE("/services/{srvid}/subscription/single/{token}").
+		Expect().
+		Status(http.StatusOK).JSON()
+}
+func TestBulkDeRegisterService(t *testing.T) {
+	var (
+		// create httpexpect instance
+		e = httpexpect.New(t, "http://localhost:8080")
+	)
+	// DELETE registries
+	e.DELETE("/services/{srvid}/registry").
+		Expect().
+		Status(http.StatusOK).JSON()
+}
+
+func TestBulkUnSubscribeService(t *testing.T) {
+	var (
+		// create httpexpect instance
+		e = httpexpect.New(t, "http://localhost:8080")
+	)
+	// DELETE subscriptions
+	e.DELETE("/services/{srvid}/subscription").
+		Expect().
+		Status(http.StatusOK).JSON()
+}
